31d171c1c71ecbdbf4b281b347ab9e2b237adc42
Michal Zarzycki <michal.p.zarzycki@wp.pl>
2020-04-30 08:12:23 -0700

    Get all user data from google

--- server/index.js
+++ server/index.js
@@ -9,9 +9,18 @@ passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
-}, accessToken => {
-    console.log(accessToken)
-}))
+    }, 
+    (accessToken, refreshToken, profile, done) => {
+        console.log('access token:', accessToken)
+        console.log('refreshToken', refreshToken)
+        console.log('profile:', profile)
+    }));
+
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}));
+
+app.get('/auth/google/callback', passport.authenticate('google'))
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
