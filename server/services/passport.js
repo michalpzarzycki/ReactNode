53e12ea9c8a8d5e582b4e8762c32941aa350680c
Michal Zarzycki <michal.p.zarzycki@wp.pl>
2020-04-30 11:03:39 -0700

    Serialize, deserialize

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,9 +1,19 @@
-const passport = require('passport');
+ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys')
-const moongose = require('mongoose')
+const mongoose = require('mongoose')
 
-const User = moongose.model('users')
+const User = mongoose.model('users')
+
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+})
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+    .then(user => {
+        done(null, user)
+    })
+})
 
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
@@ -14,9 +24,11 @@ passport.use(new GoogleStrategy({
         User.findOne({ googleID: profile.id })
         .then(existingUser => {
             if(existingUser) {
-
+                done(null, existingUser)
             } else {
-                new User({ googleID: profile.id }).save();
+                new User({ googleID: profile.id })
+                .save()
+                .then(user => done(null, user));
             }
         })
     }));
