b77b6b289849fef483dafff9d717b7e99a5c3159
Michal Zarzycki <michal.p.zarzycki@wp.pl>
2020-05-01 05:27:23 -0700

    Redux -> created store and added Provider

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,5 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from './components/App'
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+import App from './components/App';
+
+const store = createStore(() => [], {}, applyMiddleware);
+
+ReactDOM.render(
+<Provider store={store}><App /></Provider>,
+ document.querySelector('#root'));
