e404eb7e815878921e28bbd94f630935bc98e4f6
Michal Zarzycki <michal.p.zarzycki@wp.pl>
2020-05-01 06:15:01 -0700

    Removed package-lock.json and node modules bug and added installed css

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Header from './Header';
+
 const App = () => {
     return (
-        <div> Hi there</div>
+        <div>
+            <BrowserRouter>
+                <Switch>
+                    <Header />
+                </Switch>
+            </BrowserRouter>
+        </div>
     )
 }
 
